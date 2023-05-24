72122ea76c58f0b7eda20f60a4fc32b0de1da526
crysosancher <vibhupandey.vp@gmail.com>
2023-05-24 18:20:20 +0530

    tested

--- index.js
+++ index.js
@@ -7,10 +7,49 @@ const cors = require('cors');
 app.use(cors({
     origin: '*'
 }));
-app.get('/', (req, res) => {
-	res.send('Hello World!')
+const price1=async(trainNo)=>{
+  var confiq={
+    method:'GET',
+    url:`https://indian-railway-api.cyclic.app/trains/getTrain/?trainNo=${trainNo}`
+  }
+  let ms;
+  return await axios.request(confiq).then((res)=>{
+    ms=res.data;
+    //console.log(ms);
+    return ms;
+}).catch((error)=>{
+  ms="DAta error"
+  return ms; 
 })
-
+}
+const price=async(fromStation, toStation, date)=>{
+  var confiq={
+    method:'GET',
+    url:`https://indian-railway-api.cyclic.app/trains/gettrainon?from=${fromStation}&to=${toStation}&date=${date}`
+  }
+  let ms;
+  return await axios.request(confiq).then((res)=>{
+    ms=res.data;
+    //console.log(ms);
+    return ms;
+}).catch((error)=>{
+  ms="DAta error"
+  return ms; 
+})
+}
+app.get("/", (req, res) => {
+  res.send("okay")
+})
+app.get('/train',async (req, res) => {
+ const trainNo = req.query.trainNo
+  // res.json({check:1234})
+ //let k= await res.json(`${price()}`)
+ await price1(trainNo).then(data=>{
+   console.log(data);
+   res.json(data);
+ }).catch( err =>{ throw Error( `Some Error in fetching data from IRCTC: ${err} `) })
+//  await res.json(`${k}`)
+})
 app.get('/station-details',async (req, res) => {
   const [ from, tempTo, tempDate ] = req.query.fromStation.replaceAll(`'`,'').split('?');
   const to = tempTo.split('=')[1]
@@ -19,19 +58,12 @@ app.get('/station-details',async (req, res) => {
   
   // res.json({check:1234})
  //let k= await res.json(`${price()}`)
-
+ await price(from,to,date).then(data=>{
+   console.log(data);
+   res.json(data);
+ }).catch( err =>{ throw Error( `Some Error in fetching data from IRCTC: ${err} `) })
 //  await res.json(`${k}`)
 })
-app.get('/train',async (req, res) => {
-	const trainNo = req.query.trainNo
-	 // res.json({check:1234})
-	//let k= await res.json(`${price()}`)
-	await price(trainNo).then(data=>{
-		console.log(data);
-		res.json(data);
-	}).catch( err =>{ throw Error( `Some Error in fetching data from IRCTC: ${err} `) })
- //  await res.json(`${k}`)
- })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
